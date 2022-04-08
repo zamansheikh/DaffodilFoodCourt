605b6a37dea3384c028dbff706cb0faedd5c940f
Zaman Sheikh <zaman6545@gmail.com>
2022-04-08 19:58:40 +0600

    hi

--- src/Food.java
+++ src/Food.java
@@ -15,10 +15,10 @@ public class Food {
 
     public static void foodMenu(){
 		System.out.println("Employee Menu: ");
-		System.out.println("\t1. Add Employee");
-		System.out.println("\t2. Remove Employee");
-		System.out.println("\t3. Search Employee");
-		System.out.println("\t4. Display Employee");
+		System.out.println("\t1. Add Food");
+		System.out.println("\t2. Remove Food");
+		System.out.println("\t3. Search Food");
+		System.out.println("\t4. Display Food");
 		System.out.println("\t5. Back");
 		System.out.print("\tEnter your choice: ");
 	}
